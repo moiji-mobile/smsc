dec5152d962731d67b62e79b60dd14e6bc8fbba6
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-03-07 17:20:11 +0100

    travis/debian: Do not put current/last in it

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,9 +9,9 @@ fi
 OBS_PACKAGE="osmo-smsc"
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    OBS_SUBPROJECT="current"
+    OBS_SUBPROJECT=""
 else
-    OBS_SUBPROJECT="${TRAVIS_BRANCH}"
+    OBS_SUBPROJECT="${TRAVIS_BRANCH}:"
 fi
 
 cat <<- EOF > ~/.oscrc
@@ -23,11 +23,11 @@ user = ${OBS_USER}
 pass = ${OBS_PASS}
 EOF
 
-OBS_HOME="home:${OBS_USER}:${OBS_PACKAGE}:${OBS_SUBPROJECT}:latest/${OBS_PACKAGE}"
+OBS_HOME="home:${OBS_USER}:${OBS_PACKAGE}:${OBS_SUBPROJECT}latest/${OBS_PACKAGE}"
 
 cd ../
 # the project is always created via web or cli
-osc co home:${OBS_USER}:${OBS_PACKAGE}:${OBS_SUBPROJECT}:latest ${OBS_PACKAGE}
+osc co home:${OBS_USER}:${OBS_PACKAGE}:${OBS_SUBPROJECT}latest ${OBS_PACKAGE}
 
 pushd .
 # rm files if directory is not empty
