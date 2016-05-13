b33eaaef15b60ee1e8886aeca052031c2969396e
Henning Heinold <henning@itconsulting-heinold.de>
2016-05-13 22:03:46 +0200

    squash increase wait time for the inserter to come up

--- integration-tests/integration-tests.sh
+++ integration-tests/integration-tests.sh
@@ -30,7 +30,7 @@ docker run --rm --net integration-tests -v $PWD/integration-tests:/integration-t
 # inserter test
 docker run --rm --net integration-tests --name integration-tests integration-tests curl -s -S -H 'Content-Type: application/json' -XPUT http://${DOCKER_OSMO_SMSC_OM_NAME}:1700/v1/inserterSMPPLink/insertertest -d '{"connectionType": "server", "port": 9000, "systemId": "inserter-test", "systemType": "systemType", "password": "pass", "allowedRemoteAddress": null, "allowedRemotePort": null}'
 docker run -d --security-opt seccomp:unconfined --net integration-tests --name ${DOCKER_OSMO_SMSC_INSERTER_NAME} osmo-smsc osmo-smsc inserter
-sleep 3
+sleep 5
 docker run --rm --security-opt seccomp:unconfined --net integration-tests -v $PWD/integration-tests:/integration-tests --name integration-tests integration-tests python2 -m pytest --junitxml=junitxml/osmo-smsc-inserter-integrationtests.xml --inserter-server ${DOCKER_OSMO_SMSC_INSERTER_NAME} --inserter-server-port ${INSERTER_PORT} --inserter-system-id ${INSERTER_SYSTEM_ID} --inserter-password ${INSERTER_PASSWORD} --mongodb-server ${DOCKER_MONGODB_NAME} osmo-smsc-inserter-integrationtests.py
 
 # cleanup inserter test
