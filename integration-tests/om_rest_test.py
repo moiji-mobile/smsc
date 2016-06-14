31f84bc49af8033d8fa8b8c04aefc6a7097d98d7
Henning Heinold <henning@itconsulting-heinold.de>
2016-06-14 18:45:52 +0200

    more tests on the results from the om restapi answers

--- integration-tests/om_rest_test.py
+++ integration-tests/om_rest_test.py
@@ -16,56 +16,77 @@
 
 
 import pytest
+from operator import itemgetter
 
 CLIENT_NAME = "client"
 SERVER_NAME = "server"
 
-@pytest.fixture
-def client_data(request):
+def client_data():
     return {"connectionType": CLIENT_NAME,
             "hostname": "127.0.0.1",
             "port": 88,
             "systemId": "systemId",
             "systemType": "systemType",
-            "password": "password"}
-
-@pytest.fixture
-def server_data(request):
-    return {"connectionType": SERVER_NAME,
-            "port": 99,
-            "systemId": "systemId",
-            "systemType": "systemType",
             "password": "password",
+            "role" : None}
+
+def server_data():
+    return {"connectionType": SERVER_NAME,
+            "systemType": "systemType",
+            "allowedRemotePort": 99,
             "allowedRemoteAddress": "127.0.0.1",
-            "allowedRemotePort": 99}
+            "password": "password",
+            "port": 99,
+            "role": None,
+            "systemId": "systemId"}
 
 @pytest.fixture
-def client_entry(client_data, om_rest_api):
-    om_rest_api(CLIENT_NAME).put(data=client_data)
-    return client_data
+def client_entry(om_rest_api):
+    test_data = client_data()
+    om_rest_api(CLIENT_NAME).put(data=test_data)
+    return test_data
 
 @pytest.fixture
-def server_entry(server_data, om_rest_api):
-    om_rest_api(SERVER_NAME).put(data=server_data)
-    return server_data
+def server_entry(om_rest_api):
+    test_data = server_data()
+    om_rest_api(SERVER_NAME).put(data=test_data)
+    return test_data
 
 @pytest.fixture
 def collection_entries(client_entry, server_entry):
-    return [client_data, server_data]
+    return [client_entry, server_entry]
 
 @pytest.mark.usefixtures("om_image")
 class TestOM:
 
-    def test_client_insert(self, client_data, om_rest_api):
-        om_rest_api(CLIENT_NAME).put(data=client_data)
-        om_rest_api(CLIENT_NAME).get()
+    def test_client_insert(self, client_entry, om_rest_api):
+        rest_answer = om_rest_api(CLIENT_NAME).get()
 
-    def test_server_insert(self, server_data, om_rest_api):
-        om_rest_api(SERVER_NAME).put(data=server_data)
-        om_rest_api(SERVER_NAME).get()
+        assert len(rest_answer)-1 == len(client_entry)
+
+        for key in client_entry:
+            assert rest_answer[key] == client_entry[key]
+
+        assert rest_answer['connectionName'] == CLIENT_NAME
+
+    def test_server_insert(self, server_entry, om_rest_api):
+        rest_answer = om_rest_api(SERVER_NAME).get()
+
+        assert len(rest_answer)-1 == len(server_entry)
+
+        for key in server_entry:
+            assert rest_answer[key] == server_entry[key]
+
+        assert rest_answer['connectionName'] == SERVER_NAME
 
     def test_collection_api(self, collection_entries, om_rest_collection_api):
-        om_rest_collection_api.get()
+        rest_answer = om_rest_collection_api.get()
+
+        assert len(rest_answer) == 2
+        sorted_answer = sorted(rest_answer, key=itemgetter('connectionName'))
+
+        assert sorted_answer[0]['connectionName'] == CLIENT_NAME
+        assert sorted_answer[1]['connectionName'] == SERVER_NAME
 
     def test_client_update(self, client_entry, om_rest_api):
         client_entry['port'] = 99
